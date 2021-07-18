ad2d0d8b0d8d627a3c201d7aa1d2d43c854a10cf
Rian Kurnia <kurniarian25@gmail.com>
2021-07-18 16:35:33 +0700

    Change page style
    - Add CSS and JavaScript file

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } else {
+        document.getElementById("result").innerHTML = "If you deposit " + principal +
+            ", at an interest rate of " + rate + 
+            "%.You will receive an amount of \<b\>" + interest +
+            "\</b\>,in the year " + year + ".";
+    }
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval+"%";
+}
+ 
+//https://rian-kurnia.github.io/vftvk-Simple-Interest-Calculator/
